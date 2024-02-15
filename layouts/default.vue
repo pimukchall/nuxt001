07f3f692025c3f1a4a543ec80fa1505494bc54f0
Pimuk Artharnnarong <pimuk@Pimuks-MacBook-Air.local>
2024-02-15 23:28:38 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+      <v-btn @click="gotoRegister" class="mr-2" color="primary">Sign Up</v-btn>
+      <v-btn @click="gotoLogin" color="success">Sign In</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      fixed: false,
+      clipped: false,
+      title: 'MY STORE',
+    }
+  },
+  methods: {
+    gotoLogin() {
+      this.$router.push('/auth/login')
+    },
+    gotoRegister() {
+      this.$router.push('/auth/register')
+    },
+  },
+}
+</script>
+<style scoped>
+</style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="d-flex justify-center">
+      <v-card width="500" height="400">
+        <v-card-title> Sign In </v-card-title>
+          <v-card-text>
+            <v-text-field label="Email or phone number" outlined></v-text-field>
+          <v-text-field v-model="form.password" label="Password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show1 ? 'text' : 'password'" @click:append="show1 = !show1" outlined>
+          </v-text-field>
+          <v-card-actions>
+        <v-btn @click="gotoDashbord" width="450" class="mr-2" color="success" >Sign In</v-btn>
+      </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      show1: false,
+      show2: false,
+      form: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    gotoDashbord() {
+      this.$router.push('/dashbord')
+    }
+  },
+}
+</script>
